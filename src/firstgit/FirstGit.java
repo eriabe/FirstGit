b72943c39925cec8ff35eb2238f44c8add63044e
g13901ae <g13901ae@s30439.tsuda.ac.jp>
2015-04-10 11:18:33 +0900

    FirstGit

---
+++ src/firstgit/FirstGit.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package firstgit;
+
+/**
+ *
+ * @author g13901ae
+ */
+public class FirstGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.print("Hello,World!!!");
+        
+    }
+}
